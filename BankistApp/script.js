011afaf1131b473c0cf58a77e96d52a3fb2d633e
Yunuskupucu <yunus.kupucu@gmail.com>
2023-08-03 21:07:44 +0300

    convert

--- BankistApp/script.js
+++ BankistApp/script.js
@@ -92,6 +92,15 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-//const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+const eurToUsd = 1.1;
+
+const movementsUSD = movements.map(function (mov) {
+  return mov * eurToUsd;
+});
+
+console.log(movements);
+console.log(movementsUSD);
