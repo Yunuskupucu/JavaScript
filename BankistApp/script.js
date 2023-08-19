b076f4a05be7e91a79ccfdb02e91e4aacd7f899f
Yunuskupucu <yunus.kupucu@gmail.com>
2023-08-19 11:40:50 +0300

    add loan

--- BankistApp/script.js
+++ BankistApp/script.js
@@ -174,6 +174,20 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount = '';
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 
