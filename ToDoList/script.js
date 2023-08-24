a391c8c43c79181c3334cfc7fb542b21f7d9db93
Yunuskupucu <yunus.kupucu@gmail.com>
2023-08-24 16:05:55 +0300

    buttons

---
+++ ToDoList/script.js
@@ -0,0 +1,44 @@
+const addBtn = document.querySelector("#add-btn");
+const newTaskInput = document.querySelector("#wrapper input");
+const taskContainer = document.querySelector("#tasks");
+const error = document.getElementById("error");
+const countValue = document.querySelector(".count-value");
+let taskCount = 0;
+
+const displayCount = (taskCount) => {
+  countValue.innerText = taskCount;
+};
+
+const addTask = () => {
+  const taskName = newTaskInput.value.trim();
+  error.style.display = "none";
+  if (!taskName) {
+    setTimeout(() => {
+      error.style.display = "block";
+    }, 200);
+    return;
+  }
+
+  const task = `<div class = "task">
+        <input type = "checkbox" class="task-check">
+        <span class = "taskname">${taskName}</span>
+        <button class = "edit">
+        <i class="fa-solid fa-pen-to-square"></i>
+        </button>
+        <button class = "delete">
+        <i class="fa-solid fa-trash"></i>
+        </button>
+        </div>`;
+  taskContainer.insertAdjacentHTML("beforeend", task);
+
+  const deleteButtons = document.querySelectorAll(".delete");
+  deleteButtons.forEach((button) => {
+    button.onclick = () => {
+      button.parentNode.remove();
+      taskCount = -1;
+      displayCount(taskCount);
+    };
+  });
+};
+
+addBtn.addEventListener("click", addTask);
