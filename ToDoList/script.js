2299751d184b546f1faacc72c6a0a42a4928f9b2
Yunuskupucu <yunus.kupucu@gmail.com>
2023-08-25 17:36:03 +0300

    to do list UI

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -35,10 +35,46 @@ const addTask = () => {
   deleteButtons.forEach((button) => {
     button.onclick = () => {
       button.parentNode.remove();
-      taskCount = -1;
+      taskCount -= 1;
       displayCount(taskCount);
     };
   });
+
+  const editButtons = document.querySelectorAll(".edit");
+  editButtons.forEach((editBtn) => {
+    editBtn.onclick = (e) => {
+      let targetElement = e.target;
+      if (!(e.target.className == "edit")) {
+        targetElement = e.target.parentElement;
+      }
+      newTaskInput.value = targetElement.previousElementSibling?.innerText;
+      targetElement.parentNode.remove();
+      taskCount -= 1;
+      displayCount(taskCount);
+    };
+  });
+  const tasksCheck = document.querySelectorAll(".task-check");
+  tasksCheck.forEach((checkBox) => {
+    checkBox.onchange = () => {
+      checkBox.nextElementSibling.classList.toggle("completed");
+      if (checkBox.checked) {
+        taskCount -= 1;
+      } else {
+        taskCount += 1;
+      }
+      displayCount(taskCount);
+    };
+  });
+
+  taskCount += 1;
+  displayCount(taskCount);
+  newTaskInput.value = "";
 };
 
 addBtn.addEventListener("click", addTask);
+
+window.onload = () => {
+  taskCount = 0;
+  displayCount(taskCount);
+  newTaskInput.value = "";
+};
